4442c3b2ddd32bbd047b1349ff89eec1c96c83c5
Isabelle <isasoarez21@gmail.com>
2024-09-12 21:32:40 -0300

    metas abertas

--- index.js
+++ index.js
@@ -63,18 +63,34 @@ const metasRealizadas = async () =>{
     })
 
     if(realizadas.length == 0){
-        console.log("Não existem metas realizadas :(")
+        console.log(" Não existem metas realizadas :(")
     }
 
     await select({
 
-        message: "Metas Realizadas",
+        message: " Metas Realizadas " + realizadas.length,
         choices:[...realizadas]
 
     })
 
 }
 
+const metasAbertas= async() =>{
+    const abertas = metas.filter((m)=>{
+        return metas.checked != true
+    })
+
+    if(abertas.length == 0){
+
+        console.log(" Não existem metas abertas ^^")
+        return
+    }
+
+    await select({
+        message: " Metas Abertas " + abertas.length,
+        choices:[...abertas]
+    })
+}
 
 const start = async() =>{
 
@@ -99,6 +115,10 @@ const start = async() =>{
                     value:"realizadas"
                 },
 
+                {
+                    name: "Metas Abertas",
+                    value:"abertas"
+                },
 
                 {
                     name: "Sair",
@@ -121,6 +141,10 @@ const start = async() =>{
             
             case 'realizadas':
                 await metasRealizadas()
+                break
+            
+            case 'abertas':
+                await metasAbertas()
                 break
 
             case 'sair':
